6bf6f42f38c761371669d118ee0a550bc26838f2
Mikiyas Kebede <mkebede@myfuse1.education>
2024-10-27 16:40:17 +0300

    added env file

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -40,47 +41,47 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyAkYIV90PhlDFCc7p364Mgh2Z_HeN7smuI',
-    appId: '1:728223381124:web:d67033a3332e7f894774d1',
-    messagingSenderId: '728223381124',
-    projectId: 'flutter-car-rental-5e922',
-    authDomain: 'flutter-car-rental-5e922.firebaseapp.com',
-    storageBucket: 'flutter-car-rental-5e922.appspot.com',
+  static final FirebaseOptions web = FirebaseOptions(
+    apiKey: dotenv.env['WEB_API_KEY'] ?? " ",
+    appId: dotenv.env['WEB_APP_ID'] ?? "",
+    messagingSenderId: dotenv.env['WEB_MESSAGING_SENDER_ID'] ?? "",
+    projectId: dotenv.env['WEB_PROJECT_ID'] ?? "",
+    authDomain: dotenv.env['WEB_AUTH_DOMAIN'] ?? "",
+    storageBucket: dotenv.env['WEB_STORAGE_BUCKET'] ?? "",
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDGJt5w39N3TCjUnYThYnZP8LTyzGhMhb0',
-    appId: '1:728223381124:android:e191cd7c5a7ba0cb4774d1',
-    messagingSenderId: '728223381124',
-    projectId: 'flutter-car-rental-5e922',
-    storageBucket: 'flutter-car-rental-5e922.appspot.com',
+  static final FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['ANDROID_API_KEY'] ?? " ",
+    appId: dotenv.env['ANDROID_APP_ID'] ?? " ",
+    messagingSenderId: dotenv.env['ANDROID_MESSAGING_SENDER_ID'] ?? " ",
+    projectId: dotenv.env['ANDROID_PROJECT_ID'] ?? " ",
+    storageBucket: dotenv.env['ANDROID_STORAGE_BUCKET'] ?? " ",
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBhAv9CZmHps98Or1Z4jzuggGwrMdcUx20',
-    appId: '1:728223381124:ios:8131f1c6c7b65dca4774d1',
-    messagingSenderId: '728223381124',
-    projectId: 'flutter-car-rental-5e922',
-    storageBucket: 'flutter-car-rental-5e922.appspot.com',
-    iosBundleId: 'com.example.carRentalApp',
+  static final FirebaseOptions ios = FirebaseOptions(
+    apiKey: dotenv.env['IOS_API_KEY'] ?? " ",
+    appId: dotenv.env['IOS_APP_ID'] ?? " ",
+    messagingSenderId: dotenv.env['IOS_MESSAGING_SENDER_ID'] ?? " ",
+    projectId: dotenv.env['IOS_PROJECT_ID'] ?? " ",
+    storageBucket: dotenv.env['IOS_STORAGE_BUCKET'] ?? " ",
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID'] ?? " ",
   );
 
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyBhAv9CZmHps98Or1Z4jzuggGwrMdcUx20',
-    appId: '1:728223381124:ios:8131f1c6c7b65dca4774d1',
-    messagingSenderId: '728223381124',
-    projectId: 'flutter-car-rental-5e922',
-    storageBucket: 'flutter-car-rental-5e922.appspot.com',
-    iosBundleId: 'com.example.carRentalApp',
+  static final FirebaseOptions macos = FirebaseOptions(
+    apiKey: dotenv.env['MACOS_API_KEY'] ?? " ",
+    appId: dotenv.env['MACOS_APP_ID'] ?? " ",
+    messagingSenderId: dotenv.env['MACOS_MESSAGING_SENDER_ID'] ?? " ",
+    projectId: dotenv.env['MACOS_PROJECT_ID'] ?? " ",
+    storageBucket: dotenv.env['MACOS_STORAGE_BUCKET'] ?? " ",
+    iosBundleId: dotenv.env['MACOS_BUNDLE_ID'] ?? " ",
   );
 
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyAkYIV90PhlDFCc7p364Mgh2Z_HeN7smuI',
-    appId: '1:728223381124:web:c14a33c16c8684dd4774d1',
-    messagingSenderId: '728223381124',
-    projectId: 'flutter-car-rental-5e922',
-    authDomain: 'flutter-car-rental-5e922.firebaseapp.com',
-    storageBucket: 'flutter-car-rental-5e922.appspot.com',
+  static final FirebaseOptions windows = FirebaseOptions(
+    apiKey: dotenv.env['WINDOWS_API_KEY'] ?? " ",
+    appId: dotenv.env['WINDOWS_APP_ID'] ?? " ",
+    messagingSenderId: dotenv.env['WINDOWS_MESSAGING_SENDER_ID'] ?? " ",
+    projectId: dotenv.env['WINDOWS_PROJECT_ID'] ?? " ",
+    authDomain: dotenv.env['WINDOWS_AUTH_DOMAIN'] ?? " ",
+    storageBucket: dotenv.env['WINDOWS_STORAGE_BUCKET'] ?? " ",
   );
 }
